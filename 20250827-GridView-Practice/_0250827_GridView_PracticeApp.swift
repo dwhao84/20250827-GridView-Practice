c021466488cf8dc64c212f09b98a301b88c3aee8
Da Wei Hao <dawei84@hotaileasing.com.tw>
2025-08-27 08:39:41 +0800

    Initial Commit

---
+++ 20250827-GridView-Practice/_0250827_GridView_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0250827_GridView_PracticeApp.swift
+//  20250827-GridView-Practice
+//
+//  Created by Da Wei Hao on 2025/8/27.
+//
+
+import SwiftUI
+
+@main
+struct _0250827_GridView_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
